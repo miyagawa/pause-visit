37aa297f1c8200ea776d2ec4edac3bab3d95b4b2
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-08-06 13:26:04 -0700

    Added -e option to allow updating as we parse git history

---
+++ Utils.pm
@@ -0,0 +1,22 @@
+package Utils;
+
+# Update: visit-pause /tmp/PAUSE-git -e 'use Utils; Utils::update_record(@_)'
+
+use DBI;
+use Try::Tiny;
+
+my $dbh = DBI->connect("dbi:SQLite:PAUSE.sqlite3", "", "", { RaiseError => 1, PrintError => 0 });
+
+my $insert_sth = $dbh->prepare("INSERT INTO dist_packages VALUES (?, ?, ?, ?)");
+my $update_sth = $dbh->prepare("UPDATE dist_packages SET pathname = ? WHERE package = ? AND version = ?");
+
+sub update_record {
+    my($package, $version, $pathname) = @_;
+    warn "UPDATE: $package $version -> $pathname\n";
+    my $num = version->new($version)->numify;
+    try {
+        $insert_sth->execute($package, $version, $num, $data->{pathname});
+    } catch {
+        $update_sth->execute($data->{pathname}, $package, $version);
+    };
+}
